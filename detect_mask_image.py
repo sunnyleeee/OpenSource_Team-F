710b78ba0315b715d008ac044f2fe82e166099f6
yl-zzzz <71144019+yl-zzzz@users.noreply.github.com>
2020-11-19 20:36:32 +0900

    Update detect_mask_image.py

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -1,5 +1,5 @@
 # USAGE
-# python detect_mask_image.py --image images/pic1.jpeg
+# python detect_mask_image.py
 
 # import the necessary packages
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
